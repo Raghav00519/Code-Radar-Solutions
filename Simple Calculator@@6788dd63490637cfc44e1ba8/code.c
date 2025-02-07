4ab31e3a0dc03a59ae1e1f5bf2a3186ab89caaa7
Raghav Sharma <rs2716029@gmail.com>
2025-02-07 10:58:10 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -7,28 +7,63 @@ int main() {
                 scanf("%c",&oper);
                     switch(oper){
                             case '+':
-                                        printf("%d",a+b);
-                                                    break;
-                                                            case '-':
-                                                                        printf("%d",a-b);
-                                                                                    break;
-                                                                                            case '*':
-                                                                                                        printf("%d",a*b);
-                                                                                                                    break;
-                                                                                                                            case '/':
-                                                                                                                                        if(b!=0){
-                                                                                                                                                        printf("%d",a/b);
-                                                                                                                                                                    }
-                                                                                                                                                                                else{
-                                                                                                                                                                                                printf("error");
-                                                                                                                                                                                                            }
-                                                                                                                                                                                                                        break;
-                                                                                                                                                                                                                                default:
-                                                                                                                                                                                                                                            printf("error");
-                                                                                                                                                                                                                                                }
+                                printf("%d",a+b);
+                                break;
+                            case '-':
+                                printf("%d",a-b);
+                                break;
+                            case '*':
+                                printf("%d",a*b);
+                                break;
+                            case '/':
+                                if(b!=0){
+                                    printf("%d",a/b);
+                                    }
+                                else{
+                                    printf("error");
+                                    }
+                                break;
+                            default:
+                                printf("error");
+                                                                                                                       }
 
                                                                                                                                                                                                                                                     return 0;
                                                                                                                                                                                                                                                     }
+                                                                                                                                                                                                                                                    
+
+
+
+
+
+
+
+
+
+       
+             
+               
+
+                                     
+                                         
+                                        
+                                                                          
+                                                                                    
+                                                                               
+                                                                                               
+                                                                                                         
+                                                                                                                     
+                                                                                                                         
+                                                                                                               
+                                                                                                                            
+                                                                                                                                                                           
+                                                                                                                                                                                      
+                                                                                                                                                                                                 
+                                                                                                                                                                                                             
+                                                                                                                                                                                                                            
+                                                                                                                                                                                                                     
+
+                                                                                                                                                                                                                                 
+                                                                                                                                                                                                                         
                                                                                                                                                                                                                                                     
 
 
