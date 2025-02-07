f0dd01e33eb54d21281c7ed067a76f374d8f9cf2
Raghav Sharma <rs2716029@gmail.com>
2025-02-07 11:24:07 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -10,19 +10,19 @@ int main() {
                     // Checking the number of days based on the month
                         switch(month) {
                                 case 1:  // January
-                                        case 3:  // March
-                                                case 5:  // May
-                                                        case 7:  // July
-                                                                case 8:  // August
-                                                                        case 10: // October
-                                                                                case 12: // December
-                                                                                            printf("31\n");
-                                                                                                        break;
-                                                                                                                case 4:  // April
-                                                                                                                        case 6:  // June
-                                                                                                                                case 9:  // September
-                                                                                                                                        case 11: //
-                                                                                                                                        
+                                case 3:  // March
+                                case 5:  // May
+                                case 7:  // July
+                                case 8:  // August
+                                case 10: // October
+                                case 12: // December
+                                printf("31\n");
+                                     break;
+                                case 4:  // April
+                                case 6:  // June
+                                case 9:  // September
+                                case 11: //
+
 
 
 
